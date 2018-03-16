7e407063e3e6255dccb8daeed7a58db6e734264d
Kelly Netterville <bitgrease@gmail.com>
2018-03-16 00:13:42 -0400

    Updated game with methods to find winner and fixed some of the board methods.

--- TTTgame.rb
+++ TTTgame.rb
@@ -1,8 +1,9 @@
+require 'pry'
 class Board
   INITIAL_MARKER = ' '
   def initialize
     @squares = {}
-    (1..9).each { |key| @squares[key] = INITIAL_MARKER }
+    (1..9).each { |key| @squares[key] = Square.new(INITIAL_MARKER) }
   end
 
   def get_square_at(space)
@@ -10,33 +11,38 @@ class Board
   end
 
   def set_square_at(square_number, marker)
-    @squares[square_number] = marker
+    @squares[square_number].marker = marker
   end
 
   def square_available?(square_number)
-    @squares[square_number] == ' '
-  end
-
-  def random_empty_square
-    @squares.keys.select { |key| @squares[key] == INITIAL_MARKER }.sample
+    @squares[square_number].unmarked?
   end
 
   def full?
-    !@squares.values.include?(INITIAL_MARKER)
+    @squares.none? { |_, square| square.unmarked? }
   end
 
-  def num_of_mark(marker)
-    @squares.values.count(marker)
+  def num_of_mark(player_mark)
+    @squares.count { |_, square| square.marker == player_mark}
+  end
+
+  def unmarked_keys
+    @squares.keys.select { |key| @squares[key].unmarked? }
   end
 end
 
 class Square
+  attr_accessor :marker
   def initialize(marker)
     @marker = marker
   end
 
   def to_s
     @marker
+  end
+
+  def unmarked?
+    @marker == Board::INITIAL_MARKER
   end
 end
 
@@ -76,14 +82,20 @@ class TTTGame
     puts "  #{board.get_square_at(starting_square + 2)}"
   end
 
+  def print_top_border_with_key(row_start)
+    puts "#{row_start}    |#{row_start + 1}    |#{row_start + 2}"
+  end
+
+
   def display_board
-    row_border = '     |     |'
     row_separator = '-----+-----+-----'
+    row_bottom_border = '     |     |'
     puts ''
     [1, 4, 7].each do |row_start|
-      puts row_border
+      
+      puts row_bottom_border
       print_mid_row(row_start, board)
-      puts row_border
+      print_top_border_with_key(row_start)
       puts row_separator if row_start < 7
     end
     puts ''
@@ -97,33 +109,46 @@ class TTTGame
                       [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
     winning_combos.any? do |combo|
-      combo.all? { |key| board.get_square_at(key) == HUMAN_MARKER } ||
-        combo.all? { |key| board.get_square_at(key) == COMPUTER_MARKER }
+      combo.all? { |key| board.get_square_at(key).marker == HUMAN_MARKER } ||
+        combo.all? { |key| board.get_square_at(key).marker == COMPUTER_MARKER }
     end
   end
 
   def human_moves
     square_number = nil
-    puts 'Choose a square between 1-9: '
+    puts "Select a square from one of the available spaces."
+    print "Choose one: #{board.unmarked_keys}: "
     loop do
       square_number = gets.chomp.to_i
-      break if (1..9).cover?(square_number) &&
-               board.square_available?(square_number)
-      puts "Sorry, choice must be between 1 and 9 and the square must be empty."
+      break if board.unmarked_keys.include?(square_number)
+      puts "Invalid choice. Please try again."
+      print "Choose one:#{board.unmarked_keys}: "
     end
+
 
     human.mark_square(board, square_number)
   end
 
   def computer_moves
-    computer.mark_square(board, board.random_empty_square)
+    computer.mark_square(board, board.unmarked_keys.sample)
+  end
+
+  def winner?(player)
+    winning_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+                      [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+
+    winning_combos.any? do |combo|
+      combo.all? { |key| board.get_square_at(key).marker == player.marker }
+    end
   end
 
   def find_winner_and_display_result
-    if board.num_of_mark(HUMAN_MARKER) > board.num_of_mark(COMPUTER_MARKER)
+    if winner?(human)
       puts "Human won!"
-    else
+    elsif winner?(computer)
       puts "Computer won."
+    else
+      puts "It's a TIE."
     end
   end
 
@@ -134,14 +159,15 @@ class TTTGame
   def play
     clear_screen
     display_welcome_message
+    display_board
     loop do
-      display_board
       human_moves
       break if someone_won? || board.full?
 
       computer_moves
       break if someone_won? || board.full?
       clear_screen
+      display_board
     end
     clear_screen
     display_board
